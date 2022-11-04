7378cba0e26ca820cbd516cd095902ea6014b13e
Yash Shah <yashshah1282@gmail.com>
2022-11-04 22:23:35 +0300

    Using IF Else Statements

---
+++ ifstatement.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let score: i32 = 60;
+
+    if (score < 50) {
+        println!("The boy scored less than 50% in his test");
+    } else {
+        println!("The boy scored more than 50% in his test");
+    }
+}
