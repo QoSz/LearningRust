90f6bf9813bc8130c5412e4a8db99e8ec40edd62
Yash Shah <yashshah1282@gmail.com>
2022-11-04 22:29:20 +0300

    Using Else If Statement

---
+++ ifelse.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let fname: &str = "Zo";
+    let lname: &str = "Low";
+
+    if fname == "John" {
+        println!("The first name is John");
+    } else if lname == "Doe" {
+        println!("The last name is Doe");
+    } else {
+        println!("The first name is not John and the last name is not Doe");
+    }
+
+}
